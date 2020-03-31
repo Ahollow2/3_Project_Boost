daddcfa44d4a53e3c9d5e81cebf23b0871613b59
Ahollow2 <40675596+Ahollow2@users.noreply.github.com>
2020-03-31 06:52:30 -0700

    3/30

---
+++ Assets/takeFlight.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class takeFlight : MonoBehaviour
+{
+
+    public Rigidbody rB;
+    rB = GetComponent<Rigidbody>();
+    float moveRight = 500f;
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+    }
+
+    private void processInput()
+    {
+
+        if ((Input.GetKey("a")))
+        {
+            Debug.Log("Nigga moving left");
+        }
+        else if ((Input.GetKey("d")))
+        {
+            rB.AddForce(moveRight * 0 * 0 * Time.deltaTime);
+        }
+
+        else if (Input.GetKey("space"))
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
+        
+    
+
+
